722be715beb5ef9918941919c031e74e61e627f2
RobertWagnerNdbBit <robert.wagner@ndbit.de>
2023-06-12 10:22:55 +0200

    some preparations

--- build.sh
+++ build.sh
@@ -4,4 +4,8 @@ cp "env/$environment.env" "$envFile"
 source "$envFile"
 echo "Building $environment"
 
+sudo -u postgres psql -c 'create database pssrv where NOT EXISTS (SELECT FROM pg_database WHERE datname = 'pssrv')\gexec'
+
+sh dbdeploy.sh
+
 npx ts-node-esm src/server.ts
--- src/server.ts
+++ src/server.ts
@@ -6,19 +6,14 @@ import { HTTPServer } from './HtppServer/httpserver.ts';
 
 dotenv.config({ path: '.env' });
 
-if(process.env.ENV === 'local') {
-    console.log("Local env, disabling cert validation")
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-}
-
 const port: string = process.env.APP_PORT ?? "4430";
 const host: string = process.env.APP_HOST ?? "127.0.0.1";
 
 const app = express();
 
-const httpsServ = http.createServer(app);
+const httpServ = http.createServer(app);
 
-const socket = new Socket(httpsServ);
+const socket = new Socket(httpServ);
 
 socket.initSocket();
 
@@ -30,6 +25,6 @@ server.initServer();
 
 console.log("HTTP Server initialized");
 
-httpsServ.listen(parseInt(port), host, () => {
+httpServ.listen(parseInt(port), host, () => {
     console.log(`listening on ${host}:${port}`);
 });
